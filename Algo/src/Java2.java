163202724f6720f1791060f5eb886c395c08c609
2kjin <lkjin1202@gmail.com>
2024-06-14 09:47:52 +0900

    240613 과제

---
+++ Algo/src/Java2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a = a + 100;
+        b = b % 10;
+
+        System.out.print(a + " " + b);
+    }
+}
